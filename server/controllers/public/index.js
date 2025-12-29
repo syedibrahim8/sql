6e765968813d48eb352f42e3f4bdd8fc921fe8f7
syedibrahim8 <syedibrahimofficial1@gmail.com>
2025-12-29 07:32:36 +0000

    login api

--- server/controllers/public/index.js
+++ server/controllers/public/index.js
@@ -5,15 +5,32 @@ const router = express.Router();
 
 router.post("/register",async (req,res)=>{
     try {
-        const {fullName,email,password}=req.body
-        // password = await bcrypt.hash(password,10);
+        let {fullName,email,password}=req.body
+        password = await bcrypt.hash(password,10);
         const [result] = await pool.execute(
             "INSERT INTO users (fullName,email,password) VALUES (?,?,?)",[fullName,email,password]
         );
-        res.json({msg:"Registration successfull"});
+        // console.log(result);
+        res.json({id:result.insertId});
     } catch (error) {
         console.log(error);
         res.status(500).json({msg:error})
+    }
+})
+
+router.post("/login",async(req,res)=>{
+    try {
+        const {email,password} = req.body
+        let [[regPass]] = await pool.execute(
+            "SELECT password FROM users WHERE email = ?",[email]
+        )
+        if(!regPass) return res.status(404).json({msg:"User not found"})
+        let login = await bcrypt.compare(password,regPass.password)
+        if(!login) return res.status(401).json({msg:"Invalid credentials"})
+        res.status(200).json({msg:"Login success"})
+    } catch (error) {
+       console.log(error);
+       res.status(500).json({msg:error}) 
     }
 })
 
